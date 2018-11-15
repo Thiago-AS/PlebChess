159628d7d1cfc9b0e4247a64ada29ae225d68b42
Unknown <marcosvtonin@hotmail.com>
2018-11-15 10:07:15 -0200

    Testes concertados, todos falham

---
+++ code/source/personagens.cpp
@@ -0,0 +1,41 @@
+
+#include "personagens.hpp"
+
+
+	   Personagens::Personagens(void){
+
+
+}
+	   Personagens::~Personagens(void){
+
+}
+	   // metodos getter and setters
+	   int  Personagens::setVida(unsigned int vida){
+
+}
+	   int  Personagens::setDano(unsigned int dano){
+
+}
+	   int  Personagens::setPosX(unsigned int posX){
+
+}
+	   int  Personagens::setPosY(unsigned int posY){
+
+}
+	   unsigned int  Personagens::getVida(){
+
+}
+	   unsigned int  Personagens::getDano(){
+
+}
+	   unsigned int  Personagens::getPosX(){
+
+}
+	   unsigned int  Personagens::getPosY(){
+
+}
+	   tipo_personagens  Personagens::getPersonagem(){
+
+}
+		
+
