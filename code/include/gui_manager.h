a65ce0ca5ceb7d6ea3d217efd3cadaa0a5c5be3b
Thiago-AS <thiagos.araujo09@gmail.com>
2018-11-19 14:59:30 -0200

    Adicionando gui manager como singleton

---
+++ code/include/gui_manager.h
@@ -0,0 +1,24 @@
+// "Copyright 2018"
+#ifndef GUI_MANAGER_H
+#define GUI_MANAGER_H
+
+#include "./gui.h"
+
+class GuiManager {
+ private:
+  static GuiManager* instance;
+  bool quit;
+  Gui* gui;
+  SDL_Event events;
+
+  GuiManager();
+  ~GuiManager();
+
+ public:
+  static GuiManager* Instance();
+  static void Release();
+
+  void Run();
+}
+
+#endif  // GUI_MANAGER_H
